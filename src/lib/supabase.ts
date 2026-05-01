c720ba17a359442e42835dbd00029b811af1516b
achintyasoni29-eng <achintya.soni29@gmail.com>
2026-05-01 12:01:33 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your actual Supabase URL and Anon Key
 const supabaseUrl = 'https://gxgmipwfnkfyscehbytu.supabase.co';
-const supabaseAnonKey = 'eyJhbGci...[YOUR_FULL_LONG_ANON_KEY]...';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4Z21pcHdmbmtmeXNjZWhieXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NzMyNzcsImV4cCI6MjA5MzA0OTI3N30.-Os01jNm-td3Mp0voS7ClResieQdi_30rXLXatRy5Lc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
